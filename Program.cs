36eb6bc317d8613fccb03c7a2c786106db8f1155
Aziz0307 <Aziz03072018@gmail.com>
2024-12-18 17:14:48 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ProcDZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i<5;i++)
+            {
+                int A = Convert.ToInt16(Console.ReadLine());
+                var c = OddEvenNum(A);
+                Console.WriteLine(c);
+            }
+        }
+
+        static string OddEvenNum(int a)
+        {
+            if (a % 2 == 0)
+            {
+                return "even";
+                
+            }
+            else
+            {
+                return  "odd";
+            }
+        }
+    }
+}
